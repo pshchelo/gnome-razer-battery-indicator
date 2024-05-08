3d46a21881769d6b3202cc4264c2be8ad5947955
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2024-05-09 00:20:09 +0300

    Add project skeleton
    auto-generated by `gnome-extensions create` command.
    
    The main Python script to fetch the devices info from
    OpenRazer daemon (name/type/charging status/battery level)
    works but is not called by the extension yet.

---
+++ TODO.md
@@ -0,0 +1,20 @@
+# TODO
+
+## MvP (usable for myself) - single device, hardcoded options
+- run a periodic task in a loop
+- call the python script and parse results
+- set the indicator to be a composite of icon and text
+- change text based on percentage
+- properly react to device (dis|re)connects
+
+## alpha
+- change icon based on percentage/charging status
+- show device name and more info in the menu
+- more complex icon based on device type (type + battery? only type?)
+
+## Beta (usable for others)
+- support several devices at once
+  - list devices and battery status in the menu
+- support customizations
+  - polling interval
+  - choose which device battery status to show in the main icon
---
+++ extension.js
@@ -0,0 +1,50 @@
+const GETTEXT_DOMAIN = 'my-indicator-extension';
+
+const { GObject, St } = imports.gi;
+
+const ExtensionUtils = imports.misc.extensionUtils;
+const Main = imports.ui.main;
+const PanelMenu = imports.ui.panelMenu;
+const PopupMenu = imports.ui.popupMenu;
+
+const _ = ExtensionUtils.gettext;
+
+const Indicator = GObject.registerClass(
+class Indicator extends PanelMenu.Button {
+    _init() {
+        super._init(0.0, _('My Shiny Indicator'));
+
+        this.add_child(new St.Icon({
+            icon_name: 'battery-symbolic',
+            style_class: 'system-status-icon',
+        }));
+
+        let item = new PopupMenu.PopupMenuItem(_('Show Notification'));
+        item.connect('activate', () => {
+            Main.notify(_('Whatʼs up, folks?'));
+        });
+        this.menu.addMenuItem(item);
+    }
+});
+
+class Extension {
+    constructor(uuid) {
+        this._uuid = uuid;
+
+        ExtensionUtils.initTranslations(GETTEXT_DOMAIN);
+    }
+
+    enable() {
+        this._indicator = new Indicator();
+        Main.panel.addToStatusArea(this._uuid, this._indicator);
+    }
+
+    disable() {
+        this._indicator.destroy();
+        this._indicator = null;
+    }
+}
+
+function init(meta) {
+    return new Extension(meta.uuid);
+}
